0dbedea6df49f0cc20e893edc31b3fbcdd9468e4
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-02 20:03:33 -1000

    bubbleSort basically working

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-let arr = [1,9,7,4,5];
+let arr = [1,9,7,4,5, 25 , 0, 100, 3 ];
 // [1,7,9,4,5]
 // [1,7,4,9,5]
 // [1,7,4,5,9]
 
 function compareValues(arr) {
+
   for(let i = 0; i < arr.length; i++){
     let num = arr[i];
     let nextNum = arr[ i + 1 ];
+
     if(num > nextNum){
       arr.splice(i, 2, nextNum, num);
-      changed = true;
-      // was changed
-    }else{}
+      return true;
+    }
   }
 }
 
 function bubbleSort(arr) {
-  let changed = false;
-  compareValues(arr);
+  while(compareValues(arr) === true){
+    compareValues(arr);
+  }
   return arr;
 }
 
